4e27acb645cfbe9c5733c0cf3d4e368bf7863a0b
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-25 21:07:23 +0100

    Két szám különbsége

---
+++ 006. ketszamkulonbsege/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006.ketszamkulonbsege
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérek egy számot!");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérek még egy számot!");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+            int kivonas = szam1 - szam2;
+            Console.WriteLine("A két szám különbsége: " +kivonas + "." + "\n");
+
+            Console.ReadKey();
+        }
+    }
+}
